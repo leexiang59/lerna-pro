99116761695c08e4043367627b0827da98de1947
lixiang <leexiang59@163.com>
2020-09-04 16:02:53 +0800

    lerna publish

---
+++ packages/w-helpers/index.js
@@ -0,0 +1,13 @@
+/*
+ * @Description:
+ * @Author: will
+ * @Date: 2020-09-04 14:11:59
+ * @LastEditors: will
+ * @LastEditTime: 2020-09-04 16:00:01
+ */
+
+const test = () => {
+  console.log("init");
+};
+
+export default { test };
--- packages/w-utils/index.js
+++
@@ -1,8 +0,0 @@
-/*
- * @Description:
- * @Author: will
- * @Date: 2020-09-04 14:11:59
- * @LastEditors: will
- * @LastEditTime: 2020-09-04 14:12:05
- */
-console.log("init");
