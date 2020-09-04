4b820824e5e373915e9bad7599e66d73aa0e18e2
lixiang <leexiang59@163.com>
2020-09-04 21:55:56 +0800

    feat: lerna

--- packages/w-helpers/src/setCookie.ts
+++ packages/w-helpers/src/setCookie.ts
@@ -3,14 +3,12 @@
  * @Author: will
  * @Date: 2020-09-04 20:50:49
  * @LastEditors: will
- * @LastEditTime: 2020-09-04 21:30:38
+ * @LastEditTime: 2020-09-04 21:55:40
  */
 
 export default (name: string, value: string, days: number = 30) => {
   let exp = new Date();
-
   exp.setTime(exp.getTime() + days * 24 * 60 * 60 * 1000);
-
   document.cookie =
     name + "=" + escape(value) + "; path=/;expires=" + exp.toUTCString();
 };
