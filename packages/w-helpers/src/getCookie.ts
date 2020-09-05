d811e7adc1cc8f81897b3be2a6528efadf7a476b
lixiang <leexiang59@163.com>
2020-09-05 16:33:39 +0800

    feat: w-helpers

--- packages/w-helpers/src/getCookie.ts
+++ packages/w-helpers/src/getCookie.ts
@@ -3,7 +3,7 @@
  * @Author: will
  * @Date: 2020-09-05 15:59:56
  * @LastEditors: will
- * @LastEditTime: 2020-09-05 16:00:34
+ * @LastEditTime: 2020-09-05 16:12:16
  */
 export default () => {
   return {};
